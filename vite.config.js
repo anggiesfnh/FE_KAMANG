93f041a4d35e7860b7893da1c7e4297980a78832
anggie <156629779+anggiesfnh@users.noreply.github.com>
2026-04-24 14:08:28 +0700

    Change base path from '/admin/' to '/FE_KAMANG/'

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/admin/',
+  base: '/FE_KAMANG/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
